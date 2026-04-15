24f0d2434ed46865c997d1d8a61600e28bfdd0e3
Romeoo963 <rame200226@gmail.com>
2026-04-15 19:30:08 +0300

    Adding controller ,Resource for category

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Services\CategoryService;
+use Exception;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    private $CategoryService;
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->CategoryService = $categoryService;
+    }
+    public function index()
+    {
+        try {
+            $categories = $this->CategoryService->getAllCategories();
+            $result = CategoryResource::collection($categories);
+            return ResponseHelper::SuccessResponse($result, 'data fetched successfully', 200);
+        } catch (Exception $e) {
+            return ResponseHelper::FailureResponse(null, $e->getMessage());
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
